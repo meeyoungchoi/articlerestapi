dc5d93b6488f86ecee9223f8e6a5c5f756f91353
meeyoungchoi <goodlife1359@gmail.com>
2020-08-21 17:53:42 +0900

    0821_articlerestapi_project_dto_entity_jpa_sql_mustache

--- src/main/java/com/example/articlerestapi/controller/ArticleController.java
+++ src/main/java/com/example/articlerestapi/controller/ArticleController.java
@@ -1,18 +1,28 @@
 package com.example.articlerestapi.controller;
 
-import com.example.articlerestapi.dto.ArticleForm;
-import jdk.nashorn.internal.objects.annotations.Getter;
+
+import com.example.articlerestapi.entity.Article;
+import com.example.articlerestapi.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
+@RequiredArgsConstructor//final 필드 값을 알아서 가져온다 @Autowired를 대체한다
 @Controller
 public class ArticleController {
 
+    private final ArticleRepository articleRepository;
+
     @GetMapping("/articles")
-    public String index() {
+    public String index(Model model) {
+        Iterable<Article> articles = articleRepository.findAll();
+        model.addAttribute("articles",articles);
         return "articles/index";
     }
 
@@ -21,10 +31,6 @@ public class ArticleController {
         return "articles/new";
     }
 
-    @PostMapping("/articles")
-    public String create(ArticleForm articleForm) {
-        log.info(articleForm.toString());
-        return "redirect/articles";
-    }
+
 
 }
